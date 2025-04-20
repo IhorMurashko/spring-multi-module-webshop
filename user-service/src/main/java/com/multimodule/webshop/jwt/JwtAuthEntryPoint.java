76dcac1636c698ab85b1f86c4043784fad4ee2f3
Igor <i.murashko0911@gmail.com>
2025-04-20 10:52:23 +0200

    jwt security configuration

---
+++ user-service/src/main/java/com/multimodule/webshop/jwt/JwtAuthEntryPoint.java
@@ -0,0 +1,37 @@
+package com.multimodule.webshop.jwt;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@Slf4j
+public class JwtAuthEntryPoint implements AuthenticationEntryPoint {
+
+    /**
+     * Commences an authentication scheme.
+     * <p>
+     * This method is called when an exception is thrown due to an unauthenticated user attempting to access a secured resource.
+     * It logs the error and sends a 401 Unauthorized response to the client.
+     *
+     * @param request       the HttpServletRequest in which the exception occurred.
+     * @param response      the HttpServletResponse to which the error response will be sent.
+     * @param authException the exception that caused the invocation.
+     * @throws IOException      if an input or output exception occurs.
+     * @throws ServletException if a servlet-specific error occurs.
+     */
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException)
+            throws IOException, ServletException {
+        log.error(authException.getMessage(), authException);
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+    }
+}
---
+++ user-service/src/main/java/com/multimodule/webshop/security/userDetails/SecurityModel.java
@@ -0,0 +1,98 @@
+package com.multimodule.webshop.security.userDetails;
+
+import com.multimodule.webshop.exceptions.ConsumerNotFoundException;
+import com.multimodule.webshop.model.Consumer;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public record SecurityModel(
+        Consumer consumer
+
+
+) implements UserDetails {
+
+    public SecurityModel {
+        if (consumer == null) {
+            throw new ConsumerNotFoundException("consumer cannot be null");
+        }
+    }
+
+    /**
+     * Returns the authorities granted to the user. Cannot return <code>null</code>.
+     *
+     * @return the authorities, sorted by natural key (never <code>null</code>)
+     */
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.consumer.getAuthorities();
+    }
+
+    /**
+     * Returns the password used to authenticate the user.
+     *
+     * @return the password
+     */
+    @Override
+    public String getPassword() {
+        return this.consumer.getPassword();
+    }
+
+    /**
+     * Returns the username used to authenticate the user. Cannot return
+     * <code>null</code>.
+     *
+     * @return the username (never <code>null</code>)
+     */
+    @Override
+    public String getUsername() {
+        return this.consumer.getUsername();
+    }
+
+    /**
+     * Indicates whether the user's account has expired. An expired account cannot be
+     * authenticated.
+     *
+     * @return <code>true</code> if the user's account is valid (ie non-expired),
+     * <code>false</code> if no longer valid (ie expired)
+     */
+    @Override
+    public boolean isAccountNonExpired() {
+        return this.consumer.isAccountNonExpired();
+    }
+
+    /**
+     * Indicates whether the user is locked or unlocked. A locked user cannot be
+     * authenticated.
+     *
+     * @return <code>true</code> if the user is not locked, <code>false</code> otherwise
+     */
+    @Override
+    public boolean isAccountNonLocked() {
+        return this.consumer.isAccountNonLocked();
+    }
+
+    /**
+     * Indicates whether the user's credentials (password) has expired. Expired
+     * credentials prevent authentication.
+     *
+     * @return <code>true</code> if the user's credentials are valid (ie non-expired),
+     * <code>false</code> if no longer valid (ie expired)
+     */
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return this.consumer.isCredentialsNonExpired();
+    }
+
+    /**
+     * Indicates whether the user is enabled or disabled. A disabled user cannot be
+     * authenticated.
+     *
+     * @return <code>true</code> if the user is enabled, <code>false</code> otherwise
+     */
+    @Override
+    public boolean isEnabled() {
+        return this.consumer.isEnabled();
+    }
+}
